ea260205a16a8fe9ffc8f920aacc03ffca62a1fd
MaryLynJuana <mary_juana@ukr.net>
2021-02-14 19:57:06 +0200

    Commit project

---
+++ app/src/main/java/ua/kpi/comsys/ip8313/ui/student/StudentFragment.kt
@@ -0,0 +1,23 @@
+package com.example.ip8313.ui.student
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.ip8313.R
+
+class StudentFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_student, container, false)
+        return root
+    }
+}
