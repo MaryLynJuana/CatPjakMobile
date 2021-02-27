dfa417155d011863b858b3ada60b756e4be53e0e
MaryLynJuana <mary_juana@ukr.net>
2021-02-27 20:05:07 +0200

    refactor(api): Remove unused import

--- app/src/main/java/ua/kpi/comsys/ip8313/MainActivity.kt
+++ app/src/main/java/ua/kpi/comsys/ip8313/MainActivity.kt
@@ -1,13 +1,12 @@
 package ua.kpi.comsys.ip8313
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.ip8313.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
