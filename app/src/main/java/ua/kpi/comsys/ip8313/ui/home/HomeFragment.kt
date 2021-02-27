92e4ab32e8d2934ac554c5ebe8359da6e1b8d76e
MaryLynJuana <mary_juana@ukr.net>
2021-02-27 20:07:45 +0200

    refactor(api): Correct package name and inline variable

--- app/src/main/java/ua/kpi/comsys/ip8313/ui/home/HomeFragment.kt
+++ app/src/main/java/ua/kpi/comsys/ip8313/ui/home/HomeFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.ip8313.R
+import ua.kpi.comsys.ip8313.R
 
 class HomeFragment : Fragment() {
 
@@ -14,7 +14,6 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        return root
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
