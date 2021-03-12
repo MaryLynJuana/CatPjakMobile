a2003ccb2dff2344ac1f3ab3a089c6b2bdfdce36
MaryLynJuana <mary_juana@ukr.net>
2021-03-12 12:50:49 +0200

    feat(api): Implement parsing json file into list of Book instances

--- app/src/main/java/ua/kpi/comsys/ip8313/ui/books/BooksFragment.kt
+++ app/src/main/java/ua/kpi/comsys/ip8313/ui/books/BooksFragment.kt
@@ -4,6 +4,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -26,6 +27,31 @@ class BooksFragment : Fragment() {
     private var _binding: FragmentBooksBinding? = null
     private val binding get() = _binding!!
 
+    private fun getAssetData(filename: String): ByteArray? {
+        val buffer: ByteArray
+        try {
+            val inputStream = requireContext().assets.open(filename)
+            val size = inputStream.available()
+            buffer = ByteArray(size)
+            inputStream.use { it.read(buffer) }
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            return null
+        }
+        return buffer
+    }
+
+    private fun getAssetJsonData(filename: String): String? {
+        val buffer = getAssetData(filename)
+        return String(buffer!!)
+    }
+
+    private fun getBooks() : List<Book> {
+        val data = getAssetJsonData("BooksList.txt")
+        val booksJson = JSONObject(data).optString("books")
+        return Gson().fromJson(booksJson, Array<Book>::class.java).toList()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,7 +60,14 @@ class BooksFragment : Fragment() {
         _binding = FragmentBooksBinding.inflate(inflater, container, false)
         return binding.root
     }
-    
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val books = getBooks()
+        for (book in books) {
+            println(book.title)
+        }
+    }
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
