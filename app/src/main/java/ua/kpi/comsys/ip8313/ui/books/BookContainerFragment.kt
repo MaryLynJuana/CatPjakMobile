3a26558f24f1db93462fc98a6a90f9dfbb370a5d
MaryLynJuana <mary_juana@ukr.net>
2021-04-27 18:38:09 +0300

    feat(api): Implement searching, adding and deleting book items

---
+++ app/src/main/java/ua/kpi/comsys/ip8313/ui/books/BookContainerFragment.kt
@@ -0,0 +1,41 @@
+package ua.kpi.comsys.ip8313.ui.books
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import ua.kpi.comsys.ip8313.R
+
+class BookContainerFragment: Fragment() {
+    fun showBookList() {
+        childFragmentManager.commit {
+            replace(R.id.book_container, BookListFragment())
+        }
+    }
+    fun showBookItem() {
+        childFragmentManager.commit {
+            replace(R.id.book_container, BookItemFragment())
+        }
+    }
+
+    fun showBookCreationForm() {
+        childFragmentManager.commit {
+            replace(R.id.book_container, BookCreationFragment())
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_book_container, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showBookList()
+    }
+}
